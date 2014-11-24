65c8a8283317e1b0e09e12768c32d18cdbebd127
JatWaston <support@jatwaston.com>
2014-11-24 23:46:24 +0800

    获取youku播放地址

--- Service/index.php
+++ Service/index.php
@@ -3,11 +3,13 @@
 require_once('./ImageSpider.class.php');
 require_once('./MessageResponse.class.php');
 require_once('./Youku.class.php');
+require_once('./YoukuM3U8.class.php');
 header("Content-Type: text/html;charset=utf-8");
 
-$url = "v.youku.com/v_show/id_XODMzNzAwNzcy.html";
-$data = Youku::parse($url);
-print_r($data);
+// $url = "http://v.youku.com/v_show/id_XODAwMDc5Nzg0.html";
+// $data = Youku::parse($url);
+// print_r($data);
+echo youku_m3u8::get_m3u8_url('XODAwMDc5Nzg0');
 return;
 
 $imageSpider = new ImageSpider();
