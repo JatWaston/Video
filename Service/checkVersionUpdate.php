927f6e4a7451e31e04528a0caf8d31babe0ec973
JatWaston <support@jatwaston.com>
2014-12-08 13:53:05 +0800

    完善更新接口

--- Service/checkVersionUpdate.php
+++ Service/checkVersionUpdate.php
@@ -3,11 +3,31 @@
 require_once('./MessageResponse.class.php');
 require_once('./Database.class.php');
 require_once('./commonParams.php');
+require_once('./respond.config.php');
 
 header("Content-Type: text/html;charset=utf-8");
 
 $database = Database::getInstance();
 
-echo MessageResponse::createJSONMessage(0,'获取成功',array());
+$store = $params['store'];
+$version = $params['version'];
+$sql = "SELECT * FROM `versionUpdate` WHERE `store` = '{$store}' AND `isPublish` = '1' AND `latestVersion` > '{$version}';";
+
+// $content = nl2br("1.测试内容\n 2.测试内容2\n 3.测试内容3\n");
+// $sql = "UPDATE `versionUpdate` SET `updateMessage` = '{$content}'";
+
+// $database->query($sql);
+// return;
+//echo $sql . "<br/>";
+$res = $database->fetch_obj_arr($sql);
+//var_dump($res);
+$code = SUCCESS;
+$message = "数据获取成功";
+if (empty($res)) 
+{
+	$message = "无此数据";
+	$code = FAILURE;
+}
+return MessageResponse::message($code,$message,$res);
 
 ?>
